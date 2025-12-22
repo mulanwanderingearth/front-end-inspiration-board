98bbf26c807e9c7454e2cd543d41cea0a2e36ec8
DayDayUp <bipentium25@gmail.com>
2025-12-22 13:05:57 -0800

    return for app

--- inspiration-board/src/App.jsx
+++ inspiration-board/src/App.jsx
@@ -32,4 +32,31 @@ function App() {
   )
 }
 
+  // return (
+  //       <div className="App">
+  //     <header className="App-header">
+  //       <h1>Inspiration Board</h1>
+  //     </header>
+  //     <main>
+  //       <div>
+  //         <h2 className='Selected_board'>Selected Boards</h2>
+  //       </div>
+  //       <div>
+  //         <h2 className='Board_selection'>Boards</h2>
+  //       </div>
+  //       <div>
+  //         <h2 className='Create_board'> Create A New Board</h2>
+  //       </div>
+  //       <div>
+  //         <h2 className='Card_list'> Inspiration Cards</h2>
+  //       </div>
+  //         <div>
+  //         <h2 className='Create_card'> Create A New Card</h2>
+  //       </div>
+  //       <div>
+  //         <h2 className='AI'> Get Inspired </h2>
+  //       </div>
+  //     </main>
+  //   </div>
+  // )
 export default App
