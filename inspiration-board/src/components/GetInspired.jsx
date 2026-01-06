c7bde2e82bfb315c63ee32409fcf6da82fae3edb
DayDayUp <bipentium25@gmail.com>
2026-01-05 23:35:29 -0800

    GetInspired

--- inspiration-board/src/components/GetInspired.jsx
+++ inspiration-board/src/components/GetInspired.jsx
@@ -1,2 +1,33 @@
 import PropTypes from 'prop-types';
 import './GetInspired.css';
+import starIcon from '../assets/Gemini_Generated_Image_7v7lo27v7lo27v7l.png';
+
+const GetInspired = ({onGetInspiredButton,inspirationStory,loading}) =>{
+    const handleClick = () => {
+    onGetInspiredButton();
+    };
+    return <div>
+        <button onClick={handleClick}>
+            <img
+                src={starIcon}
+                alt="Star Button"
+                className="star-button-icon"/>
+        </button>
+        {loading && <p className="placeholder">Thinking very hard… ✨</p>}
+
+        {!loading && inspirationStory && (
+        <p className="inspiration-text">{inspirationStory}</p>
+        )}
+
+        {!loading && !inspirationStory && (
+        <p className="placeholder">Click the star to get inspired ✨</p>
+)}
+    </div>
+};
+GetInspired.propTypes ={
+    onGetInspiredButton: PropTypes.func.isRequired,
+    inspirationStory:PropTypes.string.isRequired,
+    loading:PropTypes.bool.isRequired,
+}
+
+export default GetInspired;
